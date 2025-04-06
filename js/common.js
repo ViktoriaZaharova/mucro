8f961dcab77d2988f39127ff9853d515c69b53f0
Viktoria Zaharova <zaharova181114@gmail.com>
2025-04-06 22:47:45 +0300

    index

--- js/common.js
+++ js/common.js
@@ -15,6 +15,21 @@ $('.certificates-slider').slick({
   nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>'
 });
 
+$('.news-slider').slick({
+  slidesToShow: 1,
+  fade: true,
+  appendArrows: '.news-slider__nav',
+  prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-left"></use></svg></button>',
+  nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>'
+});
+
+$('.products-slider').slick({
+  slidesToShow: 2,
+  appendArrows: '.products-slider__nav',
+  prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-left"></use></svg></button>',
+  nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>'
+});
+
 Fancybox.bind("[data-fancybox]", {
   // Your custom options
 });
